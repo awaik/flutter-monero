d0e780967453dc3daf5d4a6245c9e452a85b3d96
MarketKernel <global@marketkernel.com>
2023-07-07 20:51:30 +0300

    JSON accessors - auto generation

---
+++ lib/entities/outputs_request.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'outputs_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+OutputsRequest _$OutputsRequestFromJson(Map<String, dynamic> json) =>
+    OutputsRequest(
+      json['hash'] as String?,
+    );
+
+Map<String, dynamic> _$OutputsRequestToJson(OutputsRequest instance) =>
+    <String, dynamic>{
+      'hash': instance.hash,
+    };
