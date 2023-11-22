57df4242b14f9c0b5c9f4599f47bd232d5388962
MarketKernel <global@marketkernel.com>
2023-11-22 11:30:49 +0200

    daily commit

--- src/monero_app.cpp
+++ src/monero_app.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-#include "wallet/monero_wallet_full.h"
-#include "utils/monero_utils.h"
-
-using namespace monero;
+#include "monero_flutter.h"
 
 int main(int argc, char** argv)
 {
-    monero_wallet* wallet = nullptr;
+    const char *path = "/Users/dmytro/Documents/test_wallets/wallet1";
+    const char *seed = "point nerves ungainly gather loudly theatrics october misery aphid website attire erected shelter ouch hesitate nouns suede omnibus folding last fruit upbeat haystack hedgehog gather";
     
-	std::cout << "test!" << std::endl;
+    bool wallet_exists = is_wallet_exist(path);
+	std::cout << "wallet_exists=" << wallet_exists << std::endl;
+    
+    ErrorBox error;
+    restore_wallet_from_seed(path, "", seed, 0, &error);
+    
 	return 0;
 }
